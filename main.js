a2dc682e49740c56926cdd1be973153d776a44ff
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-09 12:38:32 +0100

    add: workflow

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // start code
+
+// Processo:
+
+// 1. richiesta dati all'utente
+//      - età
+//      - num km
+// 2. calcolo costo biglietto
+//      - costo iniziale in base ai km
+//      - verifica se lo sconto è applicabile
+//      - applicazione dello sconto se applicabile
+// 3. inizializzare il num a sole due cifre decimali
+// 4. stampare risultato
